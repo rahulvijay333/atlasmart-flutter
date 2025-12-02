c56513bd24566e7dce041e85c54f88b67fa170b9
Rahul V <djrahulv337@gmail.com>
2025-12-02 12:50:27 +0530

    intial folder structure added

--- lib/main.dart
+++ lib/main.dart
@@ -10,8 +10,9 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'AtlasMart',
-      home: Scaffold(body: Center(child: Text('Hello World!'))),
+      home: Scaffold(body: Center(child: Text('Welcome to AtlasMart'))),
     );
   }
 }
